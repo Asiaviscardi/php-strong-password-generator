d44a1a04f94e187b2428bdfaedf3211d58be3b9b
Asiaviscardi <asia.viscardii@gmail.com>
2023-10-29 17:42:26 +0100

    inserimento form

---
+++ form.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Password generator</title>
+
+</head>
+
+<body>
+    <form action="" method="POST">
+        <label for="">Lunghezza della password:</label>
+        <input type="number" name="password" id="password" min="8" max="32">
+    </form>
+    
+</body>
+
+</html>
